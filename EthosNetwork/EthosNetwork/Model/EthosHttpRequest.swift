240a73ced2822f42f1d755b35284874713653a8b
Etienne Goulet-Lang <github@egouletlang.com>
2019-04-17 16:55:53 -0400

    "Added builders for the ethos request class"

--- EthosNetwork/EthosNetwork/Model/EthosHttpRequest.swift
+++ EthosNetwork/EthosNetwork/Model/EthosHttpRequest.swift
@@ -10,6 +10,7 @@ import Foundation
 
 open class EthosHttpRequest {
     
+    // MARK: - Constants & Types
     public enum ContentType: String {
         case json = "application/json"
         case propertyList = "application/x-plist"
@@ -32,6 +33,57 @@ open class EthosHttpRequest {
         case put = "PUT"
     }
     
+    // MARK: - Builders & Constructors
+    public init(url: String, method: Method = .get, headers: [String: String]? = nil,
+                parameters: [String: Any]? = nil) {
+        self.url = url
+        self.method = method
+        self.headers = headers
+        self.parameters = parameters
+    }
+    
+    open func with(networkSpinner: Bool) -> EthosHttpRequest {
+        self.showStatusNetworkSpinner = networkSpinner
+        return self
+    }
+    
+    open func with(url: String) -> EthosHttpRequest {
+        self.url = url
+        return self
+    }
+    
+    open func with(method: Method) -> EthosHttpRequest {
+        self.method = method
+        return self
+    }
+    
+    open func with(headers: [String: String]?) -> EthosHttpRequest {
+        self.headers = headers
+        return self
+    }
+    
+    open func add(header: String, value: String) -> EthosHttpRequest {
+        if headers == nil {
+            headers = [:]
+        }
+        headers?[header] = value
+        return self
+    }
+    
+    open func with(parameters: [String: Any]?) -> EthosHttpRequest {
+        self.parameters = parameters
+        return self
+    }
+    
+    open func add(parameter: String, value: Any) -> EthosHttpRequest {
+        if parameters == nil {
+            parameters = [:]
+        }
+        parameters?[parameter] = value
+        return self
+    }
+    
+    // MARK: - State variables
     open var showStatusNetworkSpinner = false
     
     open var url = ""
