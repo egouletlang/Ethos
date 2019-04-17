e001647c415b5008d2b7412f7ced40b58f3d8143
Etienne Goulet-Lang <github@egouletlang.com>
2019-04-17 19:32:36 -0400

    "Adding EthosImage pod"

---
+++ EthosImage/EthosImage/EthosImage.h
@@ -0,0 +1,19 @@
+//
+//  EthosImage.h
+//  EthosImage
+//
+//  Created by Etienne Goulet-Lang on 4/17/19.
+//  Copyright © 2019 egouletlang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EthosImage.
+FOUNDATION_EXPORT double EthosImageVersionNumber;
+
+//! Project version string for EthosImage.
+FOUNDATION_EXPORT const unsigned char EthosImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EthosImage/PublicHeader.h>
+
+
