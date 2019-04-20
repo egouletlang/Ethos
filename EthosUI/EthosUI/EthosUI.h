d9fc56e3aac678e72b829a89329bd0738a624caf
Etienne Goulet-Lang <github@egouletlang.com>
2019-04-19 21:08:17 -0400

    "Creating UI pod"

---
+++ EthosUI/EthosUI/EthosUI.h
@@ -0,0 +1,19 @@
+//
+//  EthosUI.h
+//  EthosUI
+//
+//  Created by Etienne Goulet-Lang on 4/19/19.
+//  Copyright © 2019 egouletlang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EthosUI.
+FOUNDATION_EXPORT double EthosUIVersionNumber;
+
+//! Project version string for EthosUI.
+FOUNDATION_EXPORT const unsigned char EthosUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EthosUI/PublicHeader.h>
+
+
